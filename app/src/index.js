bc8120ebed82957d53f6d97c1e37b68f1a06b30a
Elizabeth Del Monaco <edelmonaco@wpi.edu>
2019-10-30 11:24:33 -0400

    removed require(bootstrap)

--- app/src/index.js
+++ app/src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-require('bootstrap');
-require('jquery');
-require('popper.js');
-
-
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

da5f913c3983dbd20ff6311298e39442e47c4efb
WinnyCheng <ccheng2@wpi.edu>
2019-10-30 11:22:26 -0400

    circles

--- app/src/components/Landing.js
+++ app/src/components/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/landing.css'
 
 class Landing extends React.Component {
     render(){
         return (
             <div>
-                <div Class="container">
+                <div className="container">
                     <div id="Heading">
                         <div id="login">
                             <h1>Login</h1>
@@ -15,19 +16,22 @@ class Landing extends React.Component {
                         </div>
                     </div>
 
-                    <div id="Features" Class="row">
+                    <div id="Features" className="row">
                         <h1>Features</h1>
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                        <span className="dot"></span>
                     </div>
 
-                    <div id="CalSch" Class="row featurette">
+                    <div id="CalSch" className="row featurette">
                         <h1>Calendar</h1>
                     </div>
 
-                    <div id="Exercise" Class="row featurette">
+                    <div id="Exercise" className="row featurette">
                         <h1>Exercise</h1>
                     </div>
 
-                    <div id="MindPerson" Class="row featurette">
+                    <div id="MindPerson" className="row featurette">
                         <h1>Mindfulness</h1>
                     </div>
                 </div>

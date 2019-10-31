c9307076868dcd0378721ed5baf4b0efd54cbca9
WinnyCheng <ccheng2@wpi.edu>
2019-10-31 15:47:42 -0400

    router stuff in app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-// import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import ItsBedtime from './components/ItsBedtime.js';
 import Landing from './components/Landing.js';
 import Home from './components/Home.js';
 
 function App() {
   return (
-      <div>
-          <Home/>
-      </div>
+      <Router>
+          <nav>
+              <ul>
+                  <li>
+                      <Link to="/bedtimeRoutine">It's bedtime routine</Link>
+                  </li>
+              </ul>
+          </nav>
+
+          <Switch>
+              <Route path ="/bedtimeRoutine">
+                  <ItsBedtime/>
+              </Route>
+          </Switch>
+      </Router>
   );
 }
 export default App;

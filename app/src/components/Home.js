725ac6e59e842028d0e3f513cc8b1a0b3e35da55
WinnyCheng <ccheng2@wpi.edu>
2019-10-31 17:23:55 -0400

    icons for home are in circle

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -6,53 +6,48 @@ class Home extends React.Component {
         return (
             <div id="homepage" className="row">
                 <h1>Home Page</h1>
-                <div className="circleContainer row justify-content-between align-content-between">
-                    <div className="col-md-4">
+                <ul className="circle">
+                    <li>
                         <span id="calIcon_h" className="dot_h">
                             <div className="iconImages_h calIconImg_h"></div>
                         </span>
-                    </div>
-                    <div className="col-md-4">
-                        <span id="exerIcon_h" className="dot_h d-flex justify-content-center">
+                    </li>
+                    <li>
+                        <span id="exerIcon_h" className="dot_h">
                             <div className="iconImages_h exerIconImg_h"></div>
                         </span>
-                    </div>
-                    <div className="col-md-4">
-                        <span id="mindIcon_h" className="dot_h d-flex justify-content-end">
+                    </li>
+                    <li>
+                        <span id="mindIcon_h" className="dot_h">
                             <div className="iconImages_h mindIconImg_h"></div>
                         </span>
-                    </div>
-                    <div className='col-md-4'>
+                    </li>
+                    <li>
                         <span id="bedIcon" className="dot_h">
                             <div className="iconImages_h bedIconImg"></div>
                         </span>
-                    </div>
-                    <div className='col-md-4'>
-                        <span className="dot_h text-center">
-                            <p>Show weather here</p>
-                        </span>
-                    </div>
-                    <div className='col-md-4'>
+                    </li>
+                    <li>
                         <span id="coffeeIcon" className="dot_h">
                             <div className="iconImages_h coffeeIconImg"></div>
                         </span>
-                    </div>
-                    <div className='col-md-4'>
+                    </li>
+                    <li>
                         <span id="fitbitIcon" className="dot_h">
                             <div className="iconImages_h fitbitIconImg"></div>
                         </span>
-                    </div>
-                    <div className='col-md-4'>
+                    </li>
+                    <li>
                         <span id="sleepIcon" className="dot_h">
                             <div className="iconImages_h sleepIconImg"></div>
                         </span>
-                    </div>
-                    <div className='col-md-4'>
+                    </li>
+                    <li>
                         <span id="perIcon" className="dot_h">
                             <div className="iconImages_h perIconImg"></div>
                         </span>
-                    </div>
-                </div>
+                    </li>
+                </ul>
             </div>
         );
     };

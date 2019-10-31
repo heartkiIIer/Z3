02993f9bba748a9dcc374926df80696135685a52
WinnyCheng <ccheng2@wpi.edu>
2019-10-30 23:41:02 -0400

    completed landing page without complete descriptions

--- app/src/components/Landing.js
+++ app/src/components/Landing.js
@@ -27,33 +27,66 @@ class Landing extends React.Component {
                     </div>
 
                     <div id="Features" className="row">
-                        <div className='col-lg-4'>
-                            <span className="dot"></span>
-                            <h2>Organize Stuff</h2>
-                            <p>Hello</p>
+                        <div className='col-md-4 my-auto'>
+                            <span id="calIcon" className="dot">
+                                <div className="iconImages calIconImg"></div>
+                            </span>
+                            <h2>Calendar and Routines</h2>
                         </div>
-                        <div className='col-lg-4'>
-                            <span className="dot"></span>
-                            <h2>Organize Stuff</h2>
-                            <p>Hello</p>
+                        <div className='col-md-4 my-auto'>
+                            <span id="exerIcon" className="dot">
+                                <div className="iconImages exerIconImg"></div>
+                            </span>
+                            <h2>Exercise and Activity Trackers</h2>
                         </div>
-                        <div className='col-lg-4'>
-                            <span className="dot"></span>
-                            <h2>Organize Stuff</h2>
-                            <p>Hello</p>
+                        <div className='col-md-4 my-auto'>
+                            <span id="mindIcon" className="dot">
+                                <div className="iconImages mindIconImg"></div>
+                            </span>
+                            <h2>Mindfulness and Personality</h2>
                         </div>
                     </div>
 
-                    <div id="CalSch" className="row featurette">
-                        <h1>Calendar</h1>
+                    <div id="CalSch" className="row featurette featureInfo">
+                        <h1 className="featureTitle">Calendar and Routines</h1>
+                        <div className="col-md-6">
+                            <div className="image-wrapper float-left pr-3 iconImages calIconImg icon"></div>
+                            <div className="single-post-content-wrapper p-3">
+                                <h3>
+                                    Keep track of all your calendar events and plan ahead
+                                    so no more all-nighters. Let your body sleep! Set a nightly
+                                    bedtime routine and make it a habit!
+                                </h3>
+                            </div>
+                        </div>
                     </div>
 
-                    <div id="Exercise" className="row featurette">
-                        <h1>Exercise</h1>
+                    <div id="Exercise" className="row featurette featureInfo text-right">
+                        <h1 className="featureTitle">Exercise and Activity Trackers</h1>
+                        <div className="col-md-6 align-self-end">
+                            <div className="image-wrapper float-right pr-3 iconImages exerIconImg icon"></div>
+                            <div className="single-post-content-wrapper p-3">
+                                <h3>
+                                    Get walking or do some exercise! Getting fit and improve
+                                    your sleep. Two birds in one stone. Sync it up with fitbit
+                                    and let the device do all the tracking for you.
+                                </h3>
+                            </div>
+                        </div>
                     </div>
 
-                    <div id="MindPerson" className="row featurette">
-                        <h1>Mindfulness</h1>
+                    <div id="MindPerson" className="row featurette featureInfo">
+                        <h1 className="featureTitle">Mindfulness and Personality</h1>
+                        <div className="col-md-6">
+                            <div className="image-wrapper float-left pr-3 iconImages mindIconImg icon"></div>
+                            <div className="single-post-content-wrapper p-3">
+                                <h3>
+                                    Open up your mind and learning more about yourself
+                                    through our Mindfulness Modules and Personality Test
+                                    how that effects your sleep behaviors
+                                </h3>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

1d36c96bfcc052e1222d24793b6dd008fb42d073
Elizabeth Del Monaco <edelmonaco@wpi.edu>
2019-10-31 09:35:38 -0400

    ItsBedtime.css loaded

--- app/src/components/ItsBedtime.js
+++ app/src/components/ItsBedtime.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import "../styles/ItsBedtime.css";
 
+/**
+ * @author Eliazbeth Del Monaco
+ * This component renders the It's Bedtime routine page.
+ * */
 class ItsBedtime extends React.Component {
     render(){
         return (
-            <div>
+            <div class = "itsBedtime">
                 <h1>Test</h1>
                 <div className="list-group">
-                    <button type="button" className="list-group-item list-group-item-action active">
+                    <button type="button" className="list-group-item list-group-item-action">
                         Cras justo odio
                     </button>
                     <button type="button" className="list-group-item list-group-item-action">Dapibus ac facilisis in
@@ -14,7 +19,22 @@ class ItsBedtime extends React.Component {
                     <button type="button" className="list-group-item list-group-item-action">Morbi leo risus</button>
                     <button type="button" className="list-group-item list-group-item-action">Porta ac consectetur ac
                     </button>
-                    <button type="button" className="list-group-item list-group-item-action" disabled>Vestibulum at
+                    <button type="button" className="list-group-item list-group-item-action">Vestibulum at
+                        eros
+                    </button>
+                </div>
+                <div className="list-group">
+                    <button type="button" className="list-group-item list-group-item-action">
+                        <input type="checkbox" class = "customCheckbox"/>
+                        <input type="checkbox" />
+
+                    </button>
+                    <button type="button" className="list-group-item list-group-item-action">Dapibus ac facilisis in
+                    </button>
+                    <button type="button" className="list-group-item list-group-item-action">Morbi leo risus</button>
+                    <button type="button" className="list-group-item list-group-item-action">Porta ac consectetur ac
+                    </button>
+                    <button type="button" className="list-group-item list-group-item-action">Vestibulum at
                         eros
                     </button>
                 </div>

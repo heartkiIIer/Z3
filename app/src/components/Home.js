66e32e7dd1168af485e8ffccbec232d3f0ae57da
WinnyCheng <ccheng2@wpi.edu>
2019-11-07 15:17:46 -0500

    working on icons on homepage

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -5,49 +5,40 @@ class Home extends React.Component {
     render(){
         return (
             <div id="homepage" className="row">
-                <h1>Home Page</h1>
-                <ul className="circle">
-                    <li>
-                        <span id="calIcon_h" className="dot_h">
-                            <div className="iconImages_h calIconImg_h"></div>
-                        </span>
-                    </li>
-                    <li>
-                        <span id="exerIcon_h" className="dot_h">
-                            <div className="iconImages_h exerIconImg_h"></div>
-                        </span>
-                    </li>
-                    <li>
-                        <span id="mindIcon_h" className="dot_h">
-                            <div className="iconImages_h mindIconImg_h"></div>
-                        </span>
-                    </li>
-                    <li>
-                        <span id="bedIcon" className="dot_h">
-                            <div className="iconImages_h bedIconImg"></div>
-                        </span>
-                    </li>
-                    <li>
-                        <span id="coffeeIcon" className="dot_h">
-                            <div className="iconImages_h coffeeIconImg"></div>
-                        </span>
-                    </li>
-                    <li>
-                        <span id="fitbitIcon" className="dot_h">
-                            <div className="iconImages_h fitbitIconImg"></div>
-                        </span>
-                    </li>
-                    <li>
-                        <span id="sleepIcon" className="dot_h">
-                            <div className="iconImages_h sleepIconImg"></div>
-                        </span>
-                    </li>
-                    <li>
-                        <span id="perIcon" className="dot_h">
-                            <div className="iconImages_h perIconImg"></div>
-                        </span>
-                    </li>
-                </ul>
+                <div class="col-lg-6">
+                    <ul className="circle">
+                        <li>
+                            <span id="sleepIcon" className="dot_h">
+                                <div className="iconImages_h sleepIconImg"></div>
+                            </span>
+                        </li>
+                        <li>
+                            <span id="logIcon_h" className="dot_h">
+                                <div className="iconImages_h logIconImg_h"></div>
+                            </span>
+                        </li>
+                        <li>
+                            <span id="mindIcon_h" className="dot_h">
+                                <div className="iconImages_h mindIconImg_h"></div>
+                            </span>
+                        </li>
+                        <li>
+                            <span id="perIcon" className="dot_h">
+                                <div className="iconImages_h perIconImg"></div>
+                            </span>
+                        </li>
+                        <li>
+                            <span id="bedIcon" className="dot_h">
+                                <div className="iconImages_h bedIconImg"></div>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-lg-6">
+                    <div>
+                        <h2>Home</h2>
+                    </div>
+                </div>
             </div>
         );
     };

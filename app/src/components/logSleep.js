d2fdf87daafa013bde105de9653799d002d43860
Sarah Armstrong <saa11698@gmail.com>
2019-11-15 09:43:51 -0500

    log sleep

--- app/src/components/logSleep.js
+++ app/src/components/logSleep.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import "../styles/logSleep.css";
+import React, { Component } from 'react';
+import '../styles/ItsBedtime.css'
+import TimePicker from 'react-time-picker';
 
-class logSleep extends React.Component{
+class LogSleep extends React.Component{
+    state = {
+        time: '10:00',
+    }
+
+    onChange = time => this.setState({ time })
+
     render(){
         return (
-            <div class = "content">
+            <div class = "content settings">
                 <div class = "inner">
-                    <div class = "itsBedtime">
-                        <h1> a </h1>
+                    <h1 class = "blueHeader" align='center'>Log Sleep</h1>
+                    <div class = "time" align='center'>
+                        <h3 class = "blueHeader" align='center'> When did you go to sleep?</h3>
+                        <TimePicker
+                            onChange={this.onChange}
+                            value={this.state.time}
+                        />
+                    </div>
+                    <div className="time" align='center'>
+                        <h3 className="blueHeader" align='center'> When did you wake up?</h3>
+                        <TimePicker
+                            onChange={this.onChange}
+                            value={this.state.time}
+                        />
+                    </div>
+                    <div className="time" align='center'>
+                        <h3 className="blueHeader" align='center'>Total time asleep:</h3>
                     </div>
                 </div>
             </div>
         );
     };
 }
-
-export default logSleep;
+export default LogSleep;

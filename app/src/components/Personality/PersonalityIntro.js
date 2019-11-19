c50fe672f7684808e272f016cca781c2f1e233dc
WinnyCheng <ccheng2@wpi.edu>
2019-11-19 18:10:15 -0500

    personality, chronotype

--- app/src/components/Personality/PersonalityIntro.js
+++ app/src/components/Personality/PersonalityIntro.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../../styles/ItsBedtime.css";
+import "../../styles/personalityIntro.css";
 import {Link} from 'react-router-dom';
 
 /**
@@ -14,17 +15,20 @@ class PersonalityIntro extends React.Component {
 
     render(){
         return (
-            <div class = "content settings">
+            <div class = "content personality">
                 <div class = "inner">
                     <h1 className="blueHeader">What is Your Personality?</h1>
                     <hr className="hr-settings"/>
-                        <h5>
-                            Take this FREE quiz. It will tell you how open, conscientious,
-                            agreeable, extraverted, and neurotic you are. This will help
-                            personalize the SleepHealth app specifically for you!
-                            When you are done taking the quiz, report your scores for the
-                            Big Five Personality Test
-                        </h5>
+                    <h2 className="blueHeader">What does "Big Five Personality" mean?</h2>
+                    <h5>
+                        The Big Five encompasses five characteristics: openness, conscientiousness,
+                        extraversion, agreeableness, and neuroticism.<br/><br/>
+                        Take this FREE quiz. It will tell you how open, conscientious,
+                        agreeable, extraverted, and neurotic you are. This will help
+                        personalize the Z<sup>3</sup> app specifically for you!
+                        When you are done taking the quiz, report your scores for the
+                        Big Five Personality Test
+                    </h5>
                     <Link to="/personality">
                         <button>Personailty Test</button>
                     </Link>

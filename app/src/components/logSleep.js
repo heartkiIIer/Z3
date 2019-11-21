41f0d8ad6122c25127919c3da82a69928a9859e3
Sarah Armstrong <saa11698@gmail.com>
2019-11-21 10:55:21 -0500

    pages

--- app/src/components/logSleep.js
+++ app/src/components/logSleep.js
@@ -4,7 +4,7 @@ import '../styles/logSleep.css'
 class LogSleep extends React.Component{
     render(){
         return (
-            <div class = "content">
+            <div class = "content logSleep">
                 <div class = "inner" id ="in">
                     <div class = "time" align='center'>
                         <h1 className="sleepHeader" align='center'>Ready for bed?</h1>

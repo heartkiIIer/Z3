2b9da8133adb6dab3196ec1b4a9db9dc6e8014a4
heartkiIIer <redapple.209@gmail.com>
2019-11-21 18:29:33 -0500

    added tabs

--- app/src/components/loggers.js
+++ app/src/components/loggers.js
@@ -7,16 +7,11 @@ import Taboo from "./logging";
 const logger = () =>
     <div class="content">
         <div className="inner">
-                <h1 className="blueHeader">Log Exercise & Caffeine for the day </h1>
+                <h1 className="blueHeader">Log Exercise </h1>
                 <hr className="hr-settings"/>
                 <Taboo/>
                 <button className='btn'>Submit</button>
         </div>
     </div>
-
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-// styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
 
 export default logger

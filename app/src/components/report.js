316b56e8b6a3e2b11abb11cd907a4d80ac1cf313
Sarah Armstrong <saa11698@gmail.com>
2019-11-21 01:57:24 -0500

    report

--- app/src/components/report.js
+++ app/src/components/report.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import '../styles/logSleep.css'
+import '../styles/ItsBedtime.css'
 
 class report extends React.Component{
     render(){
         return (
             <div class = "content">
                 <div class = "inner" id ="in">
+                    <h1 className="blueHeader">Monday</h1>
+                    <hr className="hr-settings"/>
+                    <p>Total Sleep:</p>
+                    <h1 className="timeHeader">8 hrs 30 min</h1>
 
+                    <h1 className="blueHeader">Tuesday</h1>
+                    <hr className="hr-settings"/>
+                    <p>Total Sleep:</p>
+                    <h1 className="blueHeader"> </h1>
+
+                    <h1 className="blueHeader">Wednesday</h1>
+                    <hr className="hr-settings"/>
+                    <p>Total Sleep:</p>
+                    <h1 className="blueHeader"> </h1>
+
+                    <h1 className="blueHeader">Thursday</h1>
+                    <hr className="hr-settings"/>
+                    <p>Total Sleep:</p>
+                    <h1 className="blueHeader"> </h1>
+
+                    <h1 className="blueHeader">Friday</h1>
+                    <hr className="hr-settings"/>
+                    <p>Total Sleep:</p>
+                    <h1 className="blueHeader"> </h1>
+
+                    <h1 className="blueHeader">Saturday</h1>
+                    <hr className="hr-settings"/>
+                    <p>Total Sleep:</p>
+                    <h1 className="blueHeader"> </h1>
+
+                    <h1 className="blueHeader">Sunday</h1>
+                    <hr className="hr-settings"/>
+                    <p>Total Sleep:</p>
+                    <h1 className="blueHeader"> </h1>
                 </div>
             </div>
         );

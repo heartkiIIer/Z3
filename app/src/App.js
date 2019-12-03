468a84695a1c4f5bd27f6bd5ab89005e50ab3e71
WinnyCheng <ccheng2@wpi.edu>
2019-12-03 02:59:23 -0500

    separate mobile page for landing

--- app/src/App.js
+++ app/src/App.js
@@ -19,6 +19,7 @@ import ExampleModule from './components/ExampleModule';
 import NotFound from './components/NotFound';
 import logWake from "./components/logWake";
 import BedtimeRoutine from "./components/ItsBedtimeRoutine";
+import MobileLanding from "./components/MobileLanding";
 
 function App() {
   return (
@@ -41,6 +42,7 @@ function App() {
               <Route path="/logging" component={Logger}/>
               <Route path="/report" component={report}/>
               <Route path="/ExampleModule" component={ExampleModule}/>
+              <Route path="/mobile" component={MobileLanding}/>
               <Route component = {NotFound}/>
           </Switch>
       </Router>

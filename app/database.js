6bf7f149b40a60500a5787c04f26e2cdf17b24af
edelmonaco <edelmonaco@wpi.edu>
2020-02-03 20:59:56 -0500

    server stuff

--- app/database.js
+++ app/database.js
@@ -8,13 +8,13 @@ const pool = new Pool({
 })
 
 //User management
-const getUsers = (request, response) => {
+const getUsers = (request, {status}) => {
     pool.query('SELECT * FROM users ORDER BY user_id ASC', (error, results) => {
         if (error) {
             throw error
         }
-        response.status(200).json(results.rows)
-    })
+        status(200).json(results.rows)
+    });
 }
 
 //Caffeine entry

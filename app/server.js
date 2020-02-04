696b4d955f85592ab5f09f24fff4a9f1318b144a
edelmonaco <edelmonaco@wpi.edu>
2020-02-03 19:28:21 -0500

    server stuff

--- app/server.js
+++ app/server.js
@@ -53,7 +53,14 @@ app.get('/user', cors(corsOptions), (req, res) => {
 });
 
 //DATABASE FUNCTIONALITY
-//app.get('/allUsers', cors(corsOptions), db.getUsers());
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/allUsers', cors(corsOptions), db.getUsers());
 
 app.listen(process.env.PORT || 5000);
 console.log("Listening on port 5000");

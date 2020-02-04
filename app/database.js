54e5b649b9f16779a937321a07ae64d783c304b6
edelmonaco <edelmonaco@wpi.edu>
2020-02-04 00:16:47 -0500

    db stuff

--- app/database.js
+++ app/database.js
@@ -31,8 +31,8 @@ function getUser(req, res, id) {
 //Add a new user
 function addUser(req, res, id, first, last) {
 
-    first = "''" + first + "''";
-    console.log("INSERT INTO Users(google_id, first_name, last_name) VALUES("+id+", " + first+", " + last+");");
+    //first = "\'" + first + "\'";
+    console.log("INSERT INTO Users(google_id, first_name, last_name) VALUES("+id+", \'" + first+"\', " + last+");");
     pool.query("INSERT INTO Users(google_id, first_name, last_name) VALUES("+id+", " + first+", " + last+");" , (error, results) => {
         if (error) {
             throw error

54f1c8935c081241102aa554832d2e3b65ffecbe
edelmonaco <edelmonaco@wpi.edu>
2020-02-09 22:40:06 -0500

    db stuff

--- app/src/components/ItsBedtimeRoutine.js
+++ app/src/components/ItsBedtimeRoutine.js
@@ -3,15 +3,12 @@ import "../styles/awesome-bootstrap-checkbox-master/awesome-bootstrap-checkbox.c
 import "../styles/ItsBedtime.css";
 import {CircularProgressbar, CircularProgressbarWithChildren, buildStyles} from 'react-circular-progressbar';
 import "react-circular-progressbar/dist/styles.css"
-import DeleteButton from "../resources/icons/minus-circle-solid.svg";
-import AddButton from "../resources/icons/plus-circle-solid.svg";
-import EmptyCheckbox from "../resources/icons/square-regular.svg";
-import CheckedBox from "../resources/icons/check-square-solid.svg";
 import SideBar from "./sideMenu";
 import MobileBedtimeRoutine from "./MobileBedtimeRoutine";
+import BedtimeProgressBar from "./BedtimeProgressBar";
 
 /**
- * @author Eliazbeth Del Monaco
+ * @author Eliazbeth Del Monaco, Sarah Armstrong
  * This component renders the It's Bedtime routine page.
  * */
 
@@ -21,6 +18,7 @@ import MobileBedtimeRoutine from "./MobileBedtimeRoutine";
 class ItsBedtimeRoutine extends React.Component {
     constructor(props){
         super(props)
+        this.getRoutine();
         var mobile;
         if(window.innerWidth >= 700){
             mobile = false;
@@ -72,39 +70,18 @@ class ItsBedtimeRoutine extends React.Component {
     }
 
     startRoutine(){
+        //if not initialized, show blank
         if(this.state.routine == null){
 
         }
 
-        if(this.state.stage == 0) {
-                document.getElementById("itsbedtime").style.display = "none";
-                document.getElementById("meditate").style.display = "";
-                document.getElementById("cycle").innerText = 'Next item';
-                var fiveMinutes = 60 * 1;
-                var interval = this.startTimer(fiveMinutes);
-                this.setState({stage : 1});
-        }
-        else if(this.state.stage == 1){
-            clearInterval(interval);
-            document.getElementById("meditate").style.display = "none";
-            document.getElementById("brushyourteeth").style.display = "";
-            this.setState({stage : 2});
-        }
-        else if(this.state.stage == 2){
-            document.getElementById("brushyourteeth").style.display = "none";
-            document.getElementById("washyourface").style.display = "";
-            this.setState({stage : 3});
-        }
-        else if(this.state.stage == 3){
-            document.getElementById("washyourface").style.display = "none";
-            document.getElementById("turnoffyourcomputer").style.display = "";
-            this.setState({stage : 4});
-        }
-        else if(this.state.stage == 4){
-            document.getElementById("turnoffyourcomputer").style.display = "none";
-            document.getElementById("youredone").style.display = "";
-            document.getElementById("button").innerHTML = '<a href = "/logSleep"><button class="btn" id = "cycle">Time for bed</button></a>';
+        //cycle through all bedtime routine
+        else{
+            //set size to amount of pieces
+            this.setState({stages: this.state.routine.size()})
+            while(this.state.stage != this.state.stages-1){
 
+            }
         }
     }
 
@@ -136,53 +113,8 @@ class ItsBedtimeRoutine extends React.Component {
                         <div class ="middle">
                             <div className="inner" id="page-wrap">
                                 <div class = "itsBedtime">
-                                    <div id = "items">
-                                        <div id = "itsbedtime" style={{  width: "300px" }}>
-                                            <CircularProgressbar value={100} text={`It's Bedtime`} styles={buildStyles({
-                                                textSize: 10
-                                            })}></CircularProgressbar>
-                                        </div>
-                                        <div id = "meditate" style={{  display: "none", width: "390px" }}>
-                                            <CircularProgressbarWithChildren value={0} styles={buildStyles({
-                                                pathColor: "mediumpurple",
-                                                textSize: 10
-                                            })}>
-                                                <div style={{ marginTop: -5 }}>
-                                                        <h1>Meditate</h1>
-                                                </div>
-                                                <div style={{ marginTop: -5 }}>
-                                                        <h1><span id = "timer">01:00</span> minutes</h1>
-                                                </div>
-                                            </CircularProgressbarWithChildren>
-                                        </div>
-                                        <div id = "brushyourteeth" style={{  display: "none", width: "390px" }}>
-                                            <CircularProgressbar value={25} text={`Brush your teeth`} styles={buildStyles({
-                                                pathColor: "mediumpurple",
-                                                textSize: 10
-                                            })}></CircularProgressbar>
-                                        </div>
-                                        <div id = "washyourface" style={{ display: "none", width: "390px" }}>
-                                            <CircularProgressbar value={50} text={`Wash your face`} styles={buildStyles({
-                                                pathColor: "mediumpurple",
-                                                textSize: 10
-                                            })}></CircularProgressbar>
-                                        </div>
-                                        <div id = "turnoffyourcomputer" style={{ display: "none", width: "390px" }}>
-                                            <CircularProgressbar value={75} text={`Turn off laptop`} styles={buildStyles({
-                                                pathColor: "mediumpurple",
-                                                textSize: 10
-                                            })}></CircularProgressbar>
-                                        </div>
-                                        <div id = "youredone" style={{ display: "none", width: "390px" }}>
-                                            <CircularProgressbar value={100} text={`You're done!`} styles={buildStyles({
-                                                pathColor: "mediumpurple",
-                                                textSize: 10
-                                            })}></CircularProgressbar>
-                                        </div>
-                                    </div>
+                                    <BedtimeProgressBar id = "items" name = "It's Bedtime" stage = {100} stages = {100} minutes = {0} timer = {false}/>
                                     <hr class = "bedtime-hr"/>
-                                    {/*<div id = "routine-progress">*/}
-                                    {/*</div>*/}
                                     <div className = "center" id = "button">
                                         <button className='btn' id = "cycle" onClick={() => this.startRoutine()}>Begin your routine</button>
                                     </div>

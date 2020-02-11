f9f2fda2170fd2ba9d56b8e0507d15af90814817
edelmonaco <edelmonaco@wpi.edu>
2020-02-10 20:32:19 -0500

    db stuff

--- app/src/components/ItsBedtimeRoutine.js
+++ app/src/components/ItsBedtimeRoutine.js
@@ -37,9 +37,7 @@ class ItsBedtimeRoutine extends React.Component {
     getRoutine(currentComponent) {
         function updateStates(r) {
             //console.log(JSON.stringify(r))
-            currentComponent.setState({routine : r.json()}).then(
-                currentComponent.setState({stages: Object.keys(this.state.routine).length})
-            )
+            currentComponent.setState({routine : r.json()}, ()=>currentComponent.setState({stages: Object.keys(this.state.routine).length}))
         }
 
         fetch('http://sleepwebapp.wpi.edu:5000/getRoutine', {

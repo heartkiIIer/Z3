2a0ed79f511fbc63863b74fc326f8a96b62092d4
Sarah Armstrong <saa11698@gmail.com>
2020-02-17 20:59:21 -0500

    log wake page

--- app/src/components/ExampleModule.js
+++ app/src/components/ExampleModule.js
@@ -53,17 +53,11 @@ class ExampleModule extends React.Component {
                         <h1 class = "blueHeader"> Mindful Eating</h1>
                         <hr class = "hr-settings"/>
                         <h2>Subtopic 1</h2>
-                        <p>I am so used to being the bad boy. I am so used to fighting Corporate that I forget that I am Corporate. Upper management, Jan made me breakfast this morning. Well, she bought the milk... It's soy! Ah, that baby is just discovering the whole wide world right now.</p>
-                        <br/>
-                         <p> It's pretty amazing, did this happen on company property? I miss the feeling of knowing you did a good job beacuse someone gives you proof of it. 'Sir, you're awesome, let me give you a plaque! What? A whole year has gone by? You need more proof? Here is a certificate.' They stopped making plaques that year, whoa. Wow. Who told you that?
+                        <p>This audio from the UCSD Center for Mindfulness will lead you in a 5 minute mindfulness meditation exersize.</p>
 
-                            It's a trap, what I wanna do, right now, is try something a little different.  I want you to say whatever you want, take it away. Your meeting.</p>
                         <hr className="hr-settings"/>
-
-                        <iframe id = "video" width="400" height="300" src="https://www.youtube.com/embed/CtOU4f3smt4" frameBorder="0"
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" align="center"
-                                allowFullScreen>
-                        </iframe>
+                        <iframe width="100%" height="450" scrolling="no" frameBorder="no" allow="autoplay"
+                                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/665221820&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
                     </div>
                     </div>
                 </div>

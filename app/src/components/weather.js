74f13a93d5842699578d1c01e0b45e7e71af9e90
WinnyCheng <ccheng2@wpi.edu>
2020-02-25 22:07:27 -0500

    weather complete

--- app/src/components/weather.js
+++ app/src/components/weather.js
@@ -6,7 +6,7 @@ class WeatherHome extends React.Component {
     }
 
     KelvintoFahrenheit(temp){
-        return (temp - 273.15) * 9 / 5 + 32;
+        return Math.floor((temp - 273.15) * 9 / 5 + 32);
     }
 
     render(){
@@ -14,10 +14,10 @@ class WeatherHome extends React.Component {
             <div>
                 <h2 className="whiteText">Today's Weather</h2>
                 <h5 className="whiteText">Location: {this.props.city}</h5>
-                <h4 className="whiteText">{this.props.weather.description}</h4>
+                <h4 className="whiteText">{this.props.weather.description.charAt(0).toUpperCase() + this.props.weather.description.slice(1)}</h4>
                 <h4 className="whiteText">Current Temperature: {this.KelvintoFahrenheit(this.props.main.temp)} F</h4>
                 <h5 className="whiteText">Feels like: {this.KelvintoFahrenheit(this.props.main.feels_like)} F</h5>
-                <h4 className="whiteText">Wind speed: {this.props.wind.speed * 2.237}</h4>
+                <h4 className="whiteText">Wind speed: {Math.floor(this.props.wind.speed * 2.237)} MPH</h4>
             </div>
         );
     };

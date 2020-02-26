64a58fa87f72ee946d69724a51cdd58253ad06b7
WinnyCheng <ccheng2@wpi.edu>
2020-02-25 22:20:51 -0500

    degree F symbol

--- app/src/components/weather.js
+++ app/src/components/weather.js
@@ -15,8 +15,8 @@ class WeatherHome extends React.Component {
                 <h2 className="whiteText">Today's Weather</h2>
                 <h5 className="whiteText">Location: {this.props.city}</h5>
                 <h4 className="whiteText" style={{marginTop: "20 px"}}>{this.props.weather.description.charAt(0).toUpperCase() + this.props.weather.description.slice(1)}</h4>
-                <h4 className="whiteText">Current Temperature: {this.KelvintoFahrenheit(this.props.main.temp)}&#8457</h4>
-                <h5 className="whiteText">Feels like: {this.KelvintoFahrenheit(this.props.main.feels_like)}&#8457</h5>
+                <h4 className="whiteText">Current Temperature: {this.KelvintoFahrenheit(this.props.main.temp)}&#x2109</h4>
+                <h5 className="whiteText">Feels like: {this.KelvintoFahrenheit(this.props.main.feels_like)}&#x2109</h5>
                 <h4 className="whiteText" style={{marginTop: "20 px"}}>Wind speed: {Math.floor(this.props.wind.speed * 2.237)} MPH</h4>
             </div>
         );

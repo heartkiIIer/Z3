ef0a12e75e641c83e582fb29d43440a3822e2af3
WinnyCheng <ccheng2@wpi.edu>
2020-02-25 20:18:19 -0500

    fething weather data

--- app/server.js
+++ app/server.js
@@ -171,19 +171,15 @@ app.post('/getWeekSleep/', cors(corsOptions), (req, res)=> {
 
 const zipcode = "01609";
 const apiKey = "4e527c0cbe65468e44c55d0cb68d6b16";
-request('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=' + apiKey, { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body.url);
-    console.log(body.explanation);
-});
+
 app.post('/getWeather/', cors(corsOptions), (req, res)=> {
     const {zipcode} = req.body;
-    // request('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=' + apiKey, { json: true }, (err, res, body) => {
-    //     if (err) { return console.log(err); }
-    //     console.log(body.url);
-    //     console.log(body.explanation);
-    //     res.status(200).send(body.explanation);
-    // });
+    request('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=' + apiKey, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(body.url);
+        console.log(body.explanation);
+        res.status(200).send(body.explanation);
+    });
     res.status(200).send("Hello");
 });
 

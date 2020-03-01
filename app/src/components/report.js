fc286e74f2cc85bf786ee69880f8200a1de9bca0
Elizabeth Del Monaco <edelmonaco@wpi.edu>
2020-03-01 13:33:20 -0500

    reports page hell

--- app/src/components/report.js
+++ app/src/components/report.js
@@ -197,22 +197,30 @@ class report extends React.Component{
 
         //Iterate through all entries and fetch only entries that are within the specified week
         for(var i = 0; i < Object.keys(this.state.caf).length; i++){
-            //check if that date is already entered
-            for(var j = 0; j < cardsToGenerate.length; j++){
-                var dateInArr = new Date(cardsToGenerate[j][0]);
-                var dateToEnter = new Date(this.state.caf[i].date);
-                if(dateInArr.getDate() == dateToEnter.getDate() && dateInArr.getFullYear() == dateToEnter.getFullYear() && dateInArr.getMonth() == dateToEnter.getMonth()){
-                    //if it is, check to see that there has already been an entry for this element
-                    if(cardsToGenerate[j].length >= 2){
-                        cardsToGenerate[j][1] = cardsToGenerate[j][1] + this.state.caf[i].cups;
+            //if empty, add to the array
+            if(cardsToGenerate.length == 0){
+                cardsToGenerate.push([this.state.caf[i].date, this.state.caf[i].cups]);
+            }
+            //otherwise
+            else{
+                //check if that date is already entered
+                for(var j = 0; j < cardsToGenerate.length; j++){
+                    var dateInArr = new Date(cardsToGenerate[j][0]);
+                    var dateToEnter = new Date(this.state.caf[i].date);
+                    if(dateInArr.getDate() == dateToEnter.getDate() && dateInArr.getFullYear() == dateToEnter.getFullYear() && dateInArr.getMonth() == dateToEnter.getMonth()){
+                        //if it is, check to see that there has already been an entry for this element
+                        if(cardsToGenerate[j].length >= 2){
+                            cardsToGenerate[j][1] = cardsToGenerate[j][1] + this.state.caf[i].cups;
+                        }
+                        //otherwise just add the element
+                        else{
+                            cardsToGenerate[j][1] = this.state.caf[i].cups;
+                        }
                     }
-                    //otherwise just add caffeine
-                    else{
-                        cardsToGenerate[j][1] = this.state.caf[i].cups;
+                    //otherwise just add a new entry
+                    else {
+                        cardsToGenerate.push([this.state.caf[i].date, this.state.caf[i].cups]);
                     }
-                }
-                else {
-                    cardsToGenerate.push([this.state.caf[i].date, this.state.caf[i].cups]);
                 }
             }
         }

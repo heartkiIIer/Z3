2d7bff4d3c12b20cb980a4d044d695529e02839e
WinnyCheng <ccheng2@wpi.edu>
2020-03-03 02:26:28 -0500

    ;-; ;-; ;-; Cries

--- app/src/scripts/FitbitScript.js
+++ app/src/scripts/FitbitScript.js
@@ -8,7 +8,7 @@ let OAUTH = "";
 if (url.includes("localhost")) {
     OAUTH = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22BG2J&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fsettings&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
 } else {
-    OAUTH = "https://sleepwebapp.wpi.edu/report#access_token=eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkJHMkoiLCJzdWIiOiI3TlY0UzIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNTgzNjgyOTc2LCJpYXQiOjE1ODMyMTkxNjJ9.ZBJZvjSbH1sAAAe7POnPOtV-MN59CFSVfa36RDktAPo&user_id=7NV4S2&scope=profile+settings+social+heartrate+weight+sleep+location+nutrition+activity&token_type=Bearer&expires_in=463814"
+    OAUTH = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22BG2J&redirect_uri=https%3A%2F%2Fsleepwebapp.wpi.edu%2Freport&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
 }
 
 let today = new Date();

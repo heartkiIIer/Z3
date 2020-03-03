5a5c3a1a90af8380aa302ef8115a48058850e0be
edelmonaco <edelmonaco@wpi.edu>
2020-03-02 20:55:11 -0500

    report hell

--- app/src/components/report.js
+++ app/src/components/report.js
@@ -241,7 +241,7 @@ class report extends React.Component{
                         <h1 className="blueHeader">Weekly Overview</h1>
                         <div className="week" class="flex-report">
                             <div className="goalProg">
-                                <CircularProgressbar value={Math.round(sleep/this.state.goal)*10} text={`${Math.round(sleep/this.state.goal)*10}%`} />
+                                <CircularProgressbar value={(sleep/this.state.goal)*10} text={`${(sleep/this.state.goal)*10}%`} />
                             </div>
                             <div className="percentage">
                                 <p>Average Sleep:</p>

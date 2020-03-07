fc5645ce29391e215ec28bb59b90be26634f1ef7
Shine Lin Thant <slinnthant@wpi.edu>
2020-03-07 16:38:59 -0500

    fitbit exercise attempt 2

--- app/src/scripts/FitbitScript.js
+++ app/src/scripts/FitbitScript.js
@@ -76,7 +76,7 @@ if (url.includes("report") && url.includes("#")) {
 
     var exerciseXhr = new XMLHttpRequest();
     // dates need to be in YYYY-MM-DD format
-    exerciseXhr.open('GET', 'https://api.fitbit.com/1/user/-/activities/list.json?afterDate=' + startdate + '&sort=asc&limit=100&offset=0');
+    exerciseXhr.open('GET', 'https://api.fitbit.com/1/user/-/activities/list.json?afterDate=2020-02-18&sort=asc&limit=100&offset=0');
     exerciseXhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
     exerciseXhr.onload = function () {
         if (exerciseXhr.status === 200) {

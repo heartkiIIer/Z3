1d51c6b13f1f2152b4c9ac73f1dea62c1e0f46e0
Sarah Armstrong <saa11698@gmail.com>
2020-03-08 03:02:03 -0400

    plesea work

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -178,10 +178,10 @@ class Home extends React.Component {
     }
 
     setSleepState(){
-        if(this.state.getAsleep()===true){
+        if(this.getAsleep()===true){
             return <Link to="/logSleep"><HomeIcon spanID={"sleepIcon_h"} iconClass={"iconImages_h sleepIconImg"} iconInfo={"Log Your Sleep"}/></Link>
         }
-        else if(this.state.getAsleep() === false){
+        else if(this.getAsleep() === false){
             return <Link to="/logWake"><HomeIcon spanID={"sleepIcon_h"} iconClass={"iconImages_h sleepIconImg"} iconInfo={"Log Your Sleep"}/></Link>
         }
         else{

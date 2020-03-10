75bf63f113ddac7912b934c1443f28c5d122b859
Shine Lin Thant <slinnthant@wpi.edu>
2020-03-09 21:11:52 -0400

    Added Page Titles

--- app/src/App.js
+++ app/src/App.js
@@ -26,6 +26,49 @@ import MindfulYoga from "./components/MindfulYoga";
 import BodyScanMeditation from "./components/BodyScanMeditation";
 import MindfulnessOverview from "./components/MindfulnessOverview";
 import MindfulEating from "./components/MindfulEating";
+import { TitleComponent } from './TitleComponent.jsx';
+
+// withTitle function
+const withTitle = ({ component: Component, title }) => {
+    return class Title extends Component {
+        render() {
+            return (
+                <React.Fragment>
+                    <TitleComponent title={title} />
+                    <Component {...this.props} />
+                </React.Fragment>
+            );
+        }
+    };
+};
+
+// Adding titles
+Landing = withTitle({ component: Landing, title: 'Welcome to Z³!' });
+BedtimeRoutine = withTitle({ component: BedtimeRoutine, title: 'Bedtime Routine – Z³' });
+Home = withTitle({ component: Home, title: 'Home – Z³' });
+Register = withTitle({ component: Register, title: 'Sign Up – Z³' });
+UserSettings = withTitle({ component: UserSettings, title: 'Settings – Z³' });
+logSleep = withTitle({ component: logSleep, title: 'Log Sleep – Z³' });
+logWake = withTitle({ component: logWake, title: 'Log Wake – Z³' });
+Chronotype = withTitle({ component: Chronotype, title: 'Chronotype Test– Z³' });
+ChronoResults = withTitle({ component: ChronoResults, title: 'Chronotype Results– Z³' });
+Personality = withTitle({ component: Personality, title: 'Personality Test – Z³' });
+PersonalityIntro = withTitle({ component: PersonalityIntro, title: 'Find Your Personality – Z³' });
+PersonalityResults = withTitle({ component: PersonalityResults, title: 'Personality Results – Z³' });
+MindfulnessModules = withTitle({ component: MindfulnessModules, title: 'Mindfulness – Z³' });
+Logging = withTitle({ component: Logging, title: 'Log Exercise, Caffeine, and Stress – Z³' });
+report = withTitle({ component: report, title: 'Report – Z³' });
+ExampleModule = withTitle({ component: ExampleModule, title: '5-minute Meditation – Z³' });
+mindfulLeadership = withTitle({ component: mindfulLeadership, title: 'Mindful Leadership – Z³' });
+MindfulYoga = withTitle({ component: MindfulYoga, title: 'Mindful Yoga – Z³' });
+BodyScanMeditation = withTitle({ component: BodyScanMeditation, title: '10-minute Medication – Z³' });
+MindfulnessOverview = withTitle({ component: MindfulnessOverview, title: 'Mindfulness Overview – Z³' });
+MindfulEating = withTitle({ component: MindfulEating, title: 'Mindful Eating – Z³' });
+MobileBedtimeRoutine = withTitle({ component: MobileBedtimeRoutine, title: 'Bedtime Routine – Z³' });
+MobileMindfulnessModules = withTitle({ component: MobileMindfulnessModules, title: 'Mindfulness Modules – Z³' });
+MobileExampleModule = withTitle({ component: MobileExampleModule, title: '5-minute Meditation – Z³' });
+MobileLanding = withTitle({ component: MobileLanding, title: 'Welcome to Z³!' });
+NotFound = withTitle({ component: NotFound, title: 'Page Not Found :\'(' });
 
 function App() {
   return (

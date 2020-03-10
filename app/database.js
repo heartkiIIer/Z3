619bf7ad867e542b90a7b001b954aa7086effc1a
WinnyCheng <ccheng2@wpi.edu>
2020-03-09 20:42:51 -0400

    no overlaps with sleepentry

--- app/database.js
+++ app/database.js
@@ -259,19 +259,19 @@ function addFitbitSleepEntryById(req, res, id, start, end) {
                 start+"', 'YYYY/MM/DD HH24:MI:SS'), TO_TIMESTAMP('"+
                 end+"', 'YYYY/MM/DD HH24:MI:SS')" + ' WHERE NOT EXISTS(SELECT 1 FROM exerciseentry WHERE'+
                 "start_sleep <= TO_TIMESTAMP('"+start
-                +", 'YYYY/MM/DD HH24:MI:SS') AND end_sleep >= TO_TIMESTAMP('"
-                +start+", 'YYYY/MM/DD HH24:MI:SS') OR start_sleep >= TO_TIMESTAMP('"
-                +end+", 'YYYY/MM/DD HH24:MI:SS') AND end_sleep <= TO_TIMESTAMP('"
-                +end+", 'YYYY/MM/DD HH24:MI:SS');" );
+                +"', 'YYYY/MM/DD HH24:MI:SS') AND end_sleep >= TO_TIMESTAMP('"
+                +start+"', 'YYYY/MM/DD HH24:MI:SS') OR start_sleep >= TO_TIMESTAMP('"
+                +end+"', 'YYYY/MM/DD HH24:MI:SS') AND end_sleep <= TO_TIMESTAMP('"
+                +end+"', 'YYYY/MM/DD HH24:MI:SS');" );
             pool.query("INSERT INTO SleepEntry(user_id, start_sleep, end_sleep) SELECT "+
                 internalId.rows[0].user_id+", TO_TIMESTAMP('"+
                 start+"', 'YYYY/MM/DD HH24:MI:SS'), TO_TIMESTAMP('"+
                 end+"', 'YYYY/MM/DD HH24:MI:SS')" + ' WHERE NOT EXISTS(SELECT 1 FROM exerciseentry WHERE'+
                 "start_sleep <= TO_TIMESTAMP('"+start
-                +", 'YYYY/MM/DD HH24:MI:SS') AND end_sleep >= TO_TIMESTAMP('"
-                +start+", 'YYYY/MM/DD HH24:MI:SS') OR start_sleep >= TO_TIMESTAMP('"
-                +end+", 'YYYY/MM/DD HH24:MI:SS') AND end_sleep <= TO_TIMESTAMP('"
-                +end+", 'YYYY/MM/DD HH24:MI:SS');" , (error, results) => {
+                +"', 'YYYY/MM/DD HH24:MI:SS') AND end_sleep >= TO_TIMESTAMP('"
+                +start+"', 'YYYY/MM/DD HH24:MI:SS') OR start_sleep >= TO_TIMESTAMP('"
+                +end+"', 'YYYY/MM/DD HH24:MI:SS') AND end_sleep <= TO_TIMESTAMP('"
+                +end+"', 'YYYY/MM/DD HH24:MI:SS');" , (error, results) => {
                 if (error) {
                     throw error
                 }

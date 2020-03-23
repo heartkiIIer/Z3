edca106ce3fa1da13a34adea86aa8f4461b389d8
Shine Lin Thant <slinnthant@wpi.edu>
2020-03-22 22:49:38 -0400

    more UI sigh

--- app/src/components/sleep.js
+++ app/src/components/sleep.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import '../styles/ItsBedtime.css'
+import '../styles/logSleep.css'
 import SideBar from "./sideMenu";
 import {getUserID} from "../scripts/login";
-import {CircularProgressbar} from "react-circular-progressbar";
 
 class sleep extends React.Component{
     constructor(props) {
         super(props);
-        if(window.innerWidth >= 700){
+        if(window.innerWidth >= 500){
             this.state = {
-                padding: '75px 75px 40px',
+                padding: '75px 75px 40px'
             };
         }
         else{
             this.state = {
-                padding: '10% 10% 5%',
+                padding: '10% 10% 5%'
             };
         }
     }
@@ -44,35 +43,22 @@ class sleep extends React.Component{
     render(){
         this.resize();
         return (
-            // <div className="content logSleep" id="App">
-            //     <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"}/>
-            //     <div className="middle">
-            //         <div className="logsleepInner" id="page-wrap">
-            //             <div class = "new-time" align='center'>
-            //                 <h2 className="newsleepHeader" align='center'>Waking up?</h2>
-            //                 <a href="/LogWake"><button className='btn'>Log wake time</button></a>
-            //                 <br/>
-            //                 <hr/>
-            //                 <br/>
-            //                 <h2 className="newsleepHeader" align='center'>Going to Sleep?</h2>
-            //                 <a href="/LogSleep"><button className='btn'>Log sleep time</button></a>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div>
             <div className="content logSleep" id="App">
                 <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"}/>
-                <div className="inner-report" id="page-wrap" align="center">
-                    <h1 className="blueHeader" align="center">Waking up?</h1>
-                    <a href="/LogWake"><button className='btn'>Log wake time</button></a>
-                    <br/>
-                    <hr/>
-                    <br/>
-                    <h1 className="blueHeader">Going to Sleep?</h1>
-                    <a href="/LogSleep"><button className='btn'>Log sleep time</button></a>
+                <div className="middle">
+                    <div className="logsleepInner" id="page-wrap">
+                        <div class = "new-time" align='center'>
+                            <h2 className="newsleepHeader" align='center'>Waking up?</h2>
+                            <a href="/LogWake"><button className='btn'>Log wake time</button></a>
+                            <br/>
+                            <hr/>
+                            <br/>
+                            <h2 className="newsleepHeader" align='center'>Going to Sleep?</h2>
+                            <a href="/LogSleep"><button className='btn'>Log sleep time</button></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-
         );
     };
 }

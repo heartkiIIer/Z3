5253b271e867cf8a60437a766e9af4fbd7e76f95
WinnyCheng <ccheng2@wpi.edu>
2020-03-26 01:00:59 -0400

    testing icons in firefox

--- app/src/components/HomeIcon.js
+++ app/src/components/HomeIcon.js
@@ -48,7 +48,7 @@ class HomeIcon extends React.Component {
                 <div style={containerStyle} className="flip-card">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
-                            <span>
+                            <span className={this.props.iconClass}>
                                 {this.props.iconClass}
                             </span>
                             {/*<div className={this.props.iconClass}>hello</div>*/}

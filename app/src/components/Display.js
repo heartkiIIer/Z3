00da9cd4c3fbce0a7f9154ad267eb8b5096886d0
Shine Lin Thant <slinnthant@wpi.edu>
2020-03-26 21:29:15 -0400

    more UI sigh

--- app/src/components/Display.js
+++ app/src/components/Display.js
@@ -359,7 +359,7 @@ function Display() {
             <div>
                 <Tab.Pane id="mainTab" style={{overflow: 'auto', maxHeight: 500 }} attached={false}>
                     <h5>Rate stress level for each event</h5>
-                    <button className='btn-info' onClick={GetMoreEvents}><RefreshIcon style={refresh}/></button>
+                    <button className='btn-info' onClick={GetEvents}><RefreshIcon style={refresh}/></button>
                     <br/><br/>
                     <i><p>Upcoming events of the day will be listed. Click the Refresh icon to unhide events and sync latest/newly added events from the calendar.</p></i>
                     <br/>

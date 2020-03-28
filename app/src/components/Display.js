680521c9446979420b75350e52c3ca5eafc26e88
Shine Lin Thant <slinnthant@wpi.edu>
2020-03-28 14:52:34 -0400

    fixing calendar bug

--- app/src/components/Display.js
+++ app/src/components/Display.js
@@ -57,7 +57,7 @@ export default class LoginControl extends React.Component {
         let ele;
 
         if (isLoggedIn) {
-            ele = <Display/>;
+            ele = <StatusSign/>;
         } else {
             ele = <LoginButton onClick={(e) => this.handleItemClick(e, 'sign-in')} />;
         }
@@ -285,90 +285,4 @@ function getStress(events) {
             }
         )
     })
-}
-
-
-async function fetchItems() {
-    const result = await ApiCalendar.listUpcomingEvents(250);
-    // console.log(result.result.items[result.result.items.length-1].start);
-    let approved = [];
-    let todayDate = new Date().getDate();
-    let todayMonth = new Date().getMonth();
-    let todayYear = new Date().getFullYear();
-    for(let i = 0; i < result.result.items.length; i++) {
-        let calEvent = result.result.items[i].start.dateTime;
-        let calDate = new Date(calEvent).getDate();
-        let calMonth = new Date(calEvent).getMonth();
-        let calYear = new Date(calEvent).getFullYear();
-        if(calDate == todayDate && calMonth == todayMonth && calYear == todayYear) {
-            approved.push(result.result.items[i])
-        }
-    }
-    return approved.map(({summary, start, end}) => ({summary, start, end}));
-}
-
-function Display() {
-    const [items, saveItems] = useState([]);
-    const isMounted = useRef(true);
-
-    let button = <LogoutButton onClick={(e) => LoginControlClass.handleItemClick(e, 'sign-out')} />;
-
-    useEffect(() => {
-        return () => {
-            isMounted.current = false;
-        };
-    }, []);
-
-    useEffect(() => {
-        (async () => {
-            const items = await fetchItems();
-            //Do not update state if component is unmounted
-            if (isMounted.current) {
-                saveItems(items);
-            }
-        })();
-    }, []);
-
-    if (items.length != 0) {
-        return (
-            <div>
-                <Tab.Pane id="mainTab" style={{overflow: 'auto', maxHeight: 500 }} attached={false}>
-                    <h5>Rate stress level for each event</h5>
-                    <button className='btn-info' onClick={() => window.location.reload()}><RefreshIcon style={refresh}/></button>
-                    <br/><br/>
-                    <i><p>Upcoming events of the day will be listed. Click the Refresh icon to unhide events and sync latest/newly added events from the calendar.</p></i>
-                    <br/><br/>
-                    {items.map(item => (
-                        <Item key={item.id} itemSum={item.summary} itemStart={item.start.dateTime} itemEnd={item.end.dateTime} />
-                    ))}
-                </Tab.Pane>
-                <div className='float_center'>
-                    <div className='child'>
-                        <button className='btn' onClick={submitStressEntry}>Submit Stress</button>
-                        {button}
-                        <br/><br/><br/>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <Tab.Pane id="mainTab" style={{overflow: 'auto', maxHeight: 500 }} attached={false}>
-                    <h5>Rate stress level for each event</h5>
-                    <button className='btn-info' onClick={() => window.location.reload()}><RefreshIcon style={refresh}/></button>
-                    <br/><br/>
-                    <i><p>Upcoming events of the day will be listed. Click the Refresh icon to unhide events and sync latest/newly added events from the calendar.</p></i>
-                    <br/>
-                    <i><p>You have no upcoming events for today.</p></i>
-                </Tab.Pane>
-                <div className='float_center'>
-                    <div className='child'>
-                        {button}
-                        <br/><br/><br/>
-                    </div>
-                </div></div>
-        )
-    }
-
 }

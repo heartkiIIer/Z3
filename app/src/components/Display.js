813f1b37c7ac1fd3c57f3ef082b66b51feb81c0a
Shine Lin Thant <slinnthant@wpi.edu>
2020-03-28 15:30:48 -0400

    fixing calendar bug

--- app/src/components/Display.js
+++ app/src/components/Display.js
@@ -33,13 +33,14 @@ export default class LoginControl extends React.Component {
     //     })
     // }
 
-    // componentDidMount(): void {
-    //     let currentComponent = this;
-    //     currentComponent.signUpdate = currentComponent.signUpdate.bind(currentComponent);
-    //     ApiCalendar.listenSign(currentComponent.signUpdate);
-    //     // document.getElementById('page-wrap').children[5].children[0].children[1].id = 'active'
-    //     // console.log(document.getElementById('active'))
-    // }
+    componentDidMount(): void {
+        // let currentComponent = this;
+        // currentComponent.signUpdate = currentComponent.signUpdate.bind(currentComponent);
+        // ApiCalendar.listenSign(currentComponent.signUpdate);
+        document.getElementById('page-wrap').children[5].children[0].children[1].id = 'active'
+        console.log(document.getElementById('active'))
+        document.getElementById('active').click()
+    }
 
     handleItemClick(event: SyntheticEvent<any>, name: string): void {
         if (name === 'sign-in') {

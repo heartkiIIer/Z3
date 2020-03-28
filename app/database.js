50265951c59abbb6151bc36d637871a5ac346d55
WinnyCheng <ccheng2@wpi.edu>
2020-03-28 15:46:15 -0400

    timestamp for calendar events

--- app/database.js
+++ app/database.js
@@ -236,7 +236,7 @@ function addStressEntriesById(req, res, id, title, year, month, day, date, value
     const promise = promiseBuildergoogleIdtoInternal(id);
     promise.then(
         function (internalId) {
-            pool.query('INSERT INTO stressEntry(user_id, event, month, day, year, dayofweek, stress, datetime) VALUES('+ internalId.rows[0].user_id +', ' +
+            pool.query('INSERT INTO stressEntry(user_id, event, month, day, year, dayofweek, stress, date) VALUES('+ internalId.rows[0].user_id +', ' +
                 "'" + title + "', " +  month + "," + day + "," + year + ", " + date + ", " + value + ", TO_TIMESTAMP('" + time + " 00:00:00', 'YYYY/MM/DD HH24:MI:SS')" +
                 ") ON CONFLICT (user_id, event, month, day, year) DO UPDATE SET stress="+value+";", (error, results) => {
                 if (error) {

e1ff11333b75a7798a5a313334f8b9d1cd0918e8
Shine Lin Thant <slinnthant@wpi.edu>
2020-03-30 06:50:14 -0400

    refactored code

--- app/src/components/Mindfulness/MindfulYoga.js
+++ app/src/components/Mindfulness/MindfulYoga.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import "../../styles/awesome-bootstrap-checkbox-master/awesome-bootstrap-checkbox.css";
 import "../../styles/ItsBedtime.css";
-import Tile from "./Tile.js";
 import SideBar from "../SideMenu";
-import MobileExampleModule from "./Mindfulness/MobileExampleModule";
 import {getUserID} from "../../scripts/login";
 
 class MindfulYoga extends React.Component {

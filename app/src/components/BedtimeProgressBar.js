8161e55c15fa04f76874a445ee54e969644e84bb
Shine Lin Thant <slinnthant@wpi.edu>
2020-04-02 23:14:55 -0400

    fixed default caffeine values

--- app/src/components/BedtimeProgressBar.js
+++ app/src/components/BedtimeProgressBar.js
@@ -122,7 +122,7 @@ class BedtimeProgressBar extends React.Component {
             if(this.props.title.length <= 12) {
                 title = <div style={{ marginTop: -5 }}><b><h1 style={h1}>{this.props.title}</h1></b></div>
             } else {
-                title = <div class='marquee' style={{ marginTop: -5 }}><b><h1 style={h1}>{this.props.title}</h1></b></div>
+                title = <div class='marquee' style={{ marginTop: -5}}><b><h1 style={h1}>{this.props.title}</h1></b></div>
             }
             timer = <div style={{ marginTop: -5 }}><h1 style={h1}><span id = "timer">{this.props.minutes.toString() + ':00' }</span> minutes</h1></div>
         }

5c32bf1f648aa1138a6264b882296dcc2a3d23de
Sarah Armstrong <saa11698@gmail.com>
2020-04-03 17:57:20 -0400

    plz

--- app/src/components/logWake.js
+++ app/src/components/logWake.js
@@ -134,7 +134,10 @@ class LogWake extends React.Component{
                         var yyyy = today.getFullYear();
                         today = yyyy + '/' + mm + '/' + dd + ' ';
                         var fullDate = new Date(today+time+':00');
+                        console.log(s);
+                        console.log(fullDate);
                         var sleepDate = new Date(s);
+                        console.log(sleepDate);
                         if(fullDate.getTime() < sleepDate.getTime){
                             swal({
                                 text: "Time entered is earlier than time you went to sleep",

bc8c69349d04e96aeed3201dbb6cf35121e16812
Shine Lin Thant <slinnthant@wpi.edu>
2020-04-06 03:57:53 -0400

    task list character limit

--- app/src/components/Display.js
+++ app/src/components/Display.js
@@ -27,7 +27,7 @@ export default class LoginControl extends React.Component {
 
     signUpdate(sign: boolean): any {
         this.setState({
-            sign
+            sign: sign
         })
     }
 

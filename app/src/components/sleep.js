a0395b8272c8f5a0020beeff8ce2a5bf6653350d
Sarah Armstrong <saa11698@gmail.com>
2020-04-07 00:22:58 -0400

    landing

--- app/src/components/sleep.js
+++ app/src/components/sleep.js
@@ -26,28 +26,6 @@ class sleep extends React.Component{
         });
     }
 
-    displayInfo(){
-        Swal.fire({
-            title: '<strong><u>Log Sleep Tips</u></strong>',
-            icon: 'info',
-            html:
-                "<ul><li>Be sure to log your sleep time before you go to bed every night.</li>" +
-                "<li>Be sure to log your wake time every morning after you wake up.</li>" +
-                "<li>It is important that you log your sleep time when you go to sleep " +
-                "<b>before</b> you log your wake time when you wake up so that your total hours of sleep " +
-                "will be calculated precisely. </li>" +
-                "<li>If you log your sleep multiple times before you go to bed, " +
-                "only the <b>latest</b> logged sleep time will be used in the calculation " +
-                "for your total hours of sleep for that night.</li></ul>",
-            showCloseButton: true,
-            focusConfirm: false,
-            confirmButtonText:
-                '<i class="fa fa-thumbs-up"></i> Great!',
-            confirmButtonAriaLabel: 'Thumbs up, great!',
-        })
-        document.getElementById('swal2-content').style.textAlign = 'initial';
-    }
-
     resize(){
         window.addEventListener('resize', ()=> {
             if(window.innerWidth < 700){
@@ -65,20 +43,12 @@ class sleep extends React.Component{
 
     render(){
         this.resize();
-        const style = {
-            fontSize: '36px',
-            color: '#3e98c7',
-            marginRight: '0px'
-        }
         return (
             <div className="content logSleep" id="App">
                 <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"}/>
                 <div className="middle">
                     <div className="inner" id="page-wrap">
                         <div class = "new-time" align='center'>
-                            <div align='end'>
-                                <a href='javascript:undefined' onClick={this.displayInfo}><i className="material-icons" style={style}>info_outline</i></a>
-                            </div>
                             <h2 className="newsleepHeader" align='center'>Waking up?</h2>
                             <a href="/LogWake"><button className='btn'>Log wake time</button></a>
                             <br/>
